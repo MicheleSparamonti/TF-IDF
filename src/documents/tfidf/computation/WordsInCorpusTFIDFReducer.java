fe95b5aea95f683cdb328ba4c98aeccae049fd71
MicheleSparamonti <michele.sparamonti@gmail.com>
2016-06-08 12:45:43 +0200

    Update WordsInCorpusTFIDFReducer.java

--- src/documents/tfidf/computation/WordsInCorpusTFIDFReducer.java
+++ src/documents/tfidf/computation/WordsInCorpusTFIDFReducer.java
@@ -16,14 +16,6 @@ public class WordsInCorpusTFIDFReducer extends Reducer<Text, Text, Text, Text> {
     public WordsInCorpusTFIDFReducer() {
     }
  
-    /**
-     * @param key is the key of the mapper
-     * @param values are all the values aggregated during the mapping phase
-     * @param context contains the context of the job run
-     *
-     *             PRECONDITION: receive a list of <word, ["doc1=n1/N1", "doc2=n2/N2"]>
-     *             POSTCONDITION: <"word@doc1,  [d/D, n/N, TF-IDF]">
-     */
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         // get the number of documents indirectly from the file-system (stored in the job name on purpose)
         int numberOfDocumentsInCorpus = Integer.parseInt(context.getJobName());
